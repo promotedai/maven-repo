5701e4d0fc8b83d193a1bd8aa6860a7e434570a0
Xingcan Cui <xingcan@promoted.ai>
2024-06-10 15:25:40 -0400

    add link

--- paimon.sh
+++ paimon.sh
@@ -1,3 +1,5 @@
+# Build from https://github.com/xccui/paimon/commit/186169287df683d3780d27e23f76629fbaa075a5
+
 mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.paimon -DartifactId=paimon-flink-1.18 -Dversion=0.9-xccui-SNAPSHOT -Dfile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-flink/paimon-flink-1.18/target/paimon-flink-1.18-0.9-xccui-SNAPSHOT.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-flink/paimon-flink-1.18/pom.xml
 
 mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.paimon -DartifactId=paimon-flink-common -Dversion=0.9-xccui-SNAPSHOT -Dfile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-flink/paimon-flink-common/target/paimon-flink-common-0.9-xccui-SNAPSHOT.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-flink/paimon-flink-common/pom.xml
