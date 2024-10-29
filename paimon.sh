785b76d38e3fb7c4e3312a5d324f4ea6023bb3e3
Xingcan Cui <xingcan@promoted.ai>
2024-10-29 17:54:24 -0400

    add missing libs

--- paimon.sh
+++ paimon.sh
@@ -2,6 +2,14 @@ mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=or
 
 mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.paimon -DartifactId=paimon-flink-common -Dversion=1.0-SNAPSHOT -Dfile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-flink/paimon-flink-common/target/paimon-flink-common-1.0-SNAPSHOT.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-flink/paimon-flink-common/pom.xml
 
+mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.paimon -DartifactId=paimon-bundle -Dversion=1.0-SNAPSHOT -Dfile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-bundle/target/paimon-bundle-1.0-SNAPSHOT.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-bundle/pom.xml
+
+mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.paimon -DartifactId=paimon-service-runtime -Dversion=1.0-SNAPSHOT -Dfile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-service/paimon-service-runtime/target/paimon-service-runtime-1.0-SNAPSHOT.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-service/paimon-service-runtime/pom.xml
+
+mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.paimon -DartifactId=paimon-service-client -Dversion=1.0-SNAPSHOT -Dfile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-service/paimon-service-client/target/paimon-service-client-1.0-SNAPSHOT.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-service/paimon-service-client/pom.xml
+
+mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.paimon -DartifactId=paimon-service -Dversion=1.0-SNAPSHOT  -Dfile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-service/pom.xml -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-service/pom.xml
+
 mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.paimon -DartifactId=paimon-flink -Dversion=1.0-SNAPSHOT  -Dfile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-flink/pom.xml -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/incubator-paimon/paimon-flink/pom.xml
 
 mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.paimon -DartifactId=paimon-parent -Dversion=1.0-SNAPSHOT  -Dfile=/Users/xingcan/IdeaProjects/incubator-paimon/pom.xml -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/incubator-paimon/pom.xml
