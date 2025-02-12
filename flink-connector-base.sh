bde54a48e304107bf4c7d451b8543394df020f79
Xingcan Cui <xingcan@promoted.ai>
2025-02-12 12:52:49 -0500

    add flink-file-sink-common

--- flink-connector-base.sh
+++ flink-connector-base.sh
@@ -4,6 +4,8 @@ mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=or
 
 mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.flink -DartifactId=flink-connector-files -Dversion=1.18-SNAPSHOT -Dfile=/Users/xingcan/IdeaProjects/flink/flink-connectors/flink-connector-files/target/flink-connector-files-1.18-SNAPSHOT.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/flink/flink-connectors/flink-connector-files/pom.xml
 
+mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.flink -DartifactId=flink-file-sink-common -Dversion=1.18-SNAPSHOT -Dfile=/Users/xingcan/IdeaProjects/flink/flink-connectors/flink-file-sink-common/target/flink-file-sink-common-1.18-SNAPSHOT.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/flink/flink-connectors/flink-file-sink-common/pom.xml
+
 mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.flink -DartifactId=flink-connectors -Dversion=1.18-SNAPSHOT  -Dfile=/Users/xingcan/IdeaProjects/flink/flink-connectors/pom.xml -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/flink/flink-connectors/pom.xml
 
 mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.flink -DartifactId=flink-parent -Dversion=1.18-SNAPSHOT  -Dfile=/Users/xingcan/IdeaProjects/flink/pom.xml -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/flink/pom.xml
