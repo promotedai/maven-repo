1651f3bc7105c45684f4adcd5e4e2ac885446489
Xingcan Cui <xingcan@promoted.ai>
2025-02-12 12:04:55 -0500

    feat: hybrid source hotfix

---
+++ flink-connector-base.sh
@@ -0,0 +1,7 @@
+# Build from https://github.com/xccui/flink/commit/dd56e7a8fdf8292b4bdf87ef1566febfd7295b54
+
+mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.flink -DartifactId=flink-connector-base -Dversion=1.18-SNAPSHOT -Dfile=/Users/xingcan/IdeaProjects/flink/flink-connectors/flink-connector-base/target/flink-connector-base-1.18-SNAPSHOT.jar -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/flink/flink-connectors/flink-connector-base/pom.xml
+
+mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.flink -DartifactId=flink-connectors -Dversion=1.18-SNAPSHOT  -Dfile=/Users/xingcan/IdeaProjects/flink/flink-connectors/pom.xml -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/flink/flink-connectors/pom.xml
+
+mvn org.apache.maven.plugins:maven-install-plugin:2.5::install-file -DgroupId=org.apache.flink -DartifactId=flink-parent -Dversion=1.18-SNAPSHOT  -Dfile=/Users/xingcan/IdeaProjects/flink/pom.xml -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true -DpomFile=/Users/xingcan/IdeaProjects/flink/pom.xml
